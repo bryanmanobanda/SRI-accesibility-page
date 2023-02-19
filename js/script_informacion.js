be46098476e6ef24d5daa11f1b7458d8b72bdcd0
Bryan Steven Manobanda <bryan.sst@hotmail.com>
2023-02-19 16:11:15 -0500

    Conexion de pantallas y implementación de detalle

--- js/script_informacion.js
+++ js/script_informacion.js
@@ -1,25 +1,30 @@
-document.getElementById('btnAbrir').addEventListener('click', function(){
-    document.getElementsByClassName('fondo_transparente')[0].style.display = 'block'
+function mostrar_detalle(){
+    document.getElementsByClassName('fondo_transparente')[0].style.display = 'block';
+    document.getElementsByClassName('cuerpo')[0].style.background = 'rgba(83, 83, 83, 0.226)';
     return false
-});
+}
 
 document.getElementsByClassName("modal_cerrar")[0].addEventListener("click",function(){
-    document.getElementsByClassName("fondo_transparente")[0].style.display = "none"
+    document.getElementsByClassName("fondo_transparente")[0].style.display = "none";
+    document.getElementsByClassName('cuerpo')[0].style.background = 'rgba(83, 83, 83, 0)';
 });
 
 window.addEventListener("resize", redimension())
-
+/*
 function redimension() {
-    var modal = document.getElementsByClassName("modal")[0]
-    if(window.outerHeight<500){
+    var modal = document.getElementsByClassName("caja")[0]
+    if(window.outerHeight<600){
         modal.style.height = "100%";
-    }else if(window.outerHeight>=900){
+    }else if(window.outerHeight>=100){
         modal.style.height = "500px";
     }
 
-    if(window.outerWidth < 1300){
+    if(window.outerWidth < 1000){
         modal.style.width = "100%";
-    }else if(window.outerWidth >= 1300){
+        modal.style.top= "0px";
+        modal.style.left= "0px";
+        modal.style.height = "100%";
+    }else if(window.outerWidth >= 1024){
         modal.style.width = "1300px";
     }
-}
+}*/
