dcd71eb48c1a269157dd8e3d15d58024d8a7df43
Rommel Masabanda <rommel.masabanda@epn.edu.ec>
2023-02-23 04:58:51 -0500

    Agregar texto alternativo, idiomas

--- js/script_index.js
+++ js/script_index.js
@@ -25,4 +25,95 @@ function toggleSubmenuOpciones() {
       submenuOp.style.display = "none";
     }
 }
+
+//Accesibilidad boton menu
+const botonMenu = document.getElementById('botonHamburguesa');
+botonMenu.addEventListener('click', () => {
+  const menuExpandido = botonMenu.getAttribute('aria-expanded') === 'true';
+  if (!menuExpandido) {
+    botonMenu.setAttribute('aria-label', 'Accionar para cerrar menu');
+    botonMenu.setAttribute('aria-expanded', 'true');
+  } else {
+    botonMenu.setAttribute('aria-label', 'Accionar para abrir menu');
+    botonMenu.setAttribute('aria-expanded', 'false');
+  }
+});
+
+//Cerrar el menu con ESC
+document.addEventListener('keydown', (evento) => {
+  var menuLateral = document.getElementById("barraLateral");
+  if (evento.key === 'Escape') {
+    menuLateral.style.display="none";
+    botonMenu.setAttribute('aria-expanded', 'false');
+  }
+});
+
+
+//Una vez abierto el menu si da un tab el foco se dirigue al primer elemento
+const primerElemento = document.getElementById('primeraOpcion');
+botonMenu.addEventListener('keydown', (event) => {
+  const teclaPresionada = event.key;
+  
+  if (teclaPresionada === 'Tab' && botonMenu.getAttribute('aria-expanded') === 'true') {
+    event.preventDefault();
+    primerElemento.focus();
+  }
+});
+
+//Accesibilidad boton Vehiculos menu lateral
+const botonVehiculo = document.getElementById('VehiculoML');
+botonVehiculo.addEventListener('click', () => {
+  const vehExpandido = botonVehiculo.getAttribute('aria-expanded') === 'true';
+  if (!vehExpandido) {
+    botonVehiculo.setAttribute('aria-label', 'Accionar para cerrar la lista de opciones');
+    botonVehiculo.setAttribute('aria-expanded', 'true');
+  } else {
+    botonVehiculo.setAttribute('aria-label', 'Accionar para abrir la lista de opciones');
+    botonVehiculo.setAttribute('aria-expanded', 'false');
+  }
+});
+
+const miBoton = document.querySelector('div[role="button"]');
+miBoton.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13 || event.keyCode === 32) {
+    opcionSubmenu();
+  }
+});
+
+//Accesibilidad boton Vehiculos Opciones Matriz
+const botonVehiculoM = document.getElementById('VehiculoMOp');
+botonVehiculoM.addEventListener('click', () => {
+  const vehExpandidoM = botonVehiculoM.getAttribute('aria-expanded') === 'true';
+  if (!vehExpandidoM) {
+    botonVehiculoM.setAttribute('aria-label', 'Accionar para cerrar la lista de opciones');
+    botonVehiculoM.setAttribute('aria-expanded', 'true');
+  } else {
+    botonVehiculoM.setAttribute('aria-label', 'Accionar para abrir la lista de opciones');
+    botonVehiculoM.setAttribute('aria-expanded', 'false');
+  }
+});
+botonVehiculoM.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13 || event.keyCode === 32) {
+    toggleSubmenuOpciones();
+  }
+});
+
+document.addEventListener('keydown', (evento) => {
+  var menuMatriz = document.getElementById("submenuOpciones");
+  var menuMatriz2 = document.getElementById("VehiculoMOp");
+  if (evento.key === 'Escape') {
+    menuMatriz.style.display="none";
+    menuMatriz2.setAttribute('aria-expanded', 'false');
+  }
+});
+
+
+function zoomIn(scale) {
+  document.body.style.zoom = scale;
+}
+function zoomOut(scale) {
+  document.body.style.zoom = scale;
+}
+
+
   
