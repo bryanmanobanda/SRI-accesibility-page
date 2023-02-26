9b98f43dee214b223a2c82d396756747abe057e6
Bryan Steven Manobanda <bryan.sst@hotmail.com>
2023-02-26 01:27:46 -0500

    Mejora de la barra de accesibilidad, de colores, de navegación y de cambio de idioma

--- js/script_consulta.js
+++ js/script_consulta.js
@@ -21,66 +21,76 @@ function cerrar_ventana(){
 
 
 function cambiarTamanoLetra(valor) {
-			var elementosH1 = document.getElementsByTagName("h1");
-			var elementosP = document.getElementsByTagName("p");
-            var elementosH2 = document.getElementsByTagName("h2");
-            var elementosA = document.getElementsByTagName("a");
-            var elementosLI = document.getElementsByTagName("li");
-            var elementosINP = document.getElementsByTagName("input");
-            var elementosH4 = document.getElementsByTagName("h4");
-            var elementosLB = document.getElementsByTagName("label");
-            var elementosBT = document.getElementsByTagName("button");
-            var elementosEM = document.getElementsByTagName("em");
+	var elementosH1 = document.getElementsByTagName("h1");
+	var elementosP = document.getElementsByTagName("p");
+	var elementosSpan = document.getElementsByTagName("span");
+	var elementosH2 = document.getElementsByTagName("h2");
+	var elementosA = document.getElementsByTagName("a");
+	var elementosLI = document.getElementsByTagName("li");
+	var elementosINP = document.getElementsByTagName("input");
+	var elementosH4 = document.getElementsByTagName("h4");
+	var elementosLB = document.getElementsByTagName("label");
+	var elementosBT = document.getElementsByTagName("button");
+	var elementosEM = document.getElementsByTagName("em");
 
-			for (var i = 0; i < elementosH1.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosH1[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosH1[i].style.fontSize = tamanoNuevo;
-			}
-			for (var i = 0; i < elementosP.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosP[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosP[i].style.fontSize = tamanoNuevo;
-			}
+	for (var i = 0; i < elementosH1.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosH1[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosH1[i].style.fontSize = tamanoNuevo;
+	}
+	for (var i = 0; i < elementosP.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosP[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosP[i].style.fontSize = tamanoNuevo;
+	}
+	for (var i = 0; i < elementosSpan.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosSpan[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosSpan[i].style.fontSize = tamanoNuevo;
+	}
 
-            for (var i = 0; i < elementosH2.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosH2[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosH2[i].style.fontSize = tamanoNuevo;
-			}
-            for (var i = 0; i < elementosA.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosA[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosA[i].style.fontSize = tamanoNuevo;
-			}
-            for (var i = 0; i < elementosLI.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosLI[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosLI[i].style.fontSize = tamanoNuevo;
-			}
-            for (var i = 0; i < elementosINP.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosINP[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosINP[i].style.fontSize = tamanoNuevo;
-			}
-            for (var i = 0; i < elementosH4.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosH4[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosH4[i].style.fontSize = tamanoNuevo;
-			}
-            for (var i = 0; i < elementosLB.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosLB[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosLB[i].style.fontSize = tamanoNuevo;
-			}
-            for (var i = 0; i < elementosBT.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosBT[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosBT[i].style.fontSize = tamanoNuevo;
-			}
-            for (var i = 0; i < elementosEM.length; i++) {
-				var tamanoActual = window.getComputedStyle(elementosEM[i], null).getPropertyValue("font-size");
-				var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
-				elementosEM[i].style.fontSize = tamanoNuevo;
-			}
-		}
+	for (var i = 0; i < elementosH2.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosH2[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosH2[i].style.fontSize = tamanoNuevo;
+	}
+	for (var i = 0; i < elementosA.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosA[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosA[i].style.fontSize = tamanoNuevo;
+	}
+	for (var i = 0; i < elementosLI.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosLI[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosLI[i].style.fontSize = tamanoNuevo;
+	}
+	for (var i = 0; i < elementosINP.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosINP[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosINP[i].style.fontSize = tamanoNuevo;
+	}
+	for (var i = 0; i < elementosH4.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosH4[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosH4[i].style.fontSize = tamanoNuevo;
+	}
+	for (var i = 0; i < elementosLB.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosLB[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosLB[i].style.fontSize = tamanoNuevo;
+	}
+	for (var i = 0; i < elementosBT.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosBT[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosBT[i].style.fontSize = tamanoNuevo;
+	}
+	for (var i = 0; i < elementosEM.length; i++) {
+		var tamanoActual = window.getComputedStyle(elementosEM[i], null).getPropertyValue("font-size");
+		var tamanoNuevo = parseInt(tamanoActual) + valor + "px";
+		elementosEM[i].style.fontSize = tamanoNuevo;
+	}
+}
+
+function setLanguage(languageCode) {
+	document.documentElement.lang = languageCode;
+  }
